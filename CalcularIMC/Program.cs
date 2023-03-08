a928be24cdcaf3364fc34c27479927516ca9bbfe
MatheusRaposo1 <matheus-raposo1@hotmail.com>
2023-03-08 10:40:23 -0300

    Add project files.

---
+++ CalcularIMC/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o peso em kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe altura em metros ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double valorIMC = peso / (altura * altura);
+
+            if (valorIMC < 20 )
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Abaixo do peso");
+            }
+            else if ((valorIMC >= 20) && (valorIMC<=24))
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Peso normal");
+            }
+            else if ((valorIMC >= 25) && (valorIMC < 30))
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Acima do Peso");
+            }
+            else if ((valorIMC >= 30) && (valorIMC < 34))
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + " -> Obesso");
+            }
+            else
+            {
+                Console.WriteLine("\n IMC = " + valorIMC + "-> Muito Obesso");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
